11a64724d067884ae37441bedd27b40c789d204e
Chris <chris_thompson@bcit.ca>
2021-12-06 21:35:59 -0800

    Add requirement to question 4

--- 4.py
+++ 4.py
@@ -10,19 +10,24 @@ the sixth by 12, and so on, until we reach the end of the message we are encrypt
 
 Such a scheme is called a Vigenère cipher and it is about 400 years old.
 
-Implement a function called vigenere which accepts the name of a plaintext file (a string), and
-a non-empty tuple of positive integers that are all in the range [0, 25].
+Implement a function called vigenere which accepts the name of a plaintext file (a string),
+a non-empty tuple of positive integers that are all in the range [0, 25] and a boolean called
+encode.
 
 The vigenere function must open the file for reading only. If the file does not exist, raise
 a suitable exception that contains a suitable message, and end the program gracefully.
 
-If the file does exist, you must encrypt the message in the file using the Vigenere cipher.
+If the file does exist, you must:
+ a) encrypt the message in the file using the Vigenere cipher if the boolean encode is True
+ b) decrupt the message in the file using the Vigenere cipher if the boolean encode is False.
 
-The vigenere function must store the encrypted message in a new file whose name is
-the original file appended with _shifted. For example, passing the file 'message' to the
-vigenere function will encode the text in message and write it to a file called 'message_shifted'.
+The vigenere function must store the shifted message in a new file whose name is
+the original file appended with _shifted. For example, passing a file called 'message' to the
+vigenere function will shift the text in message and write it to a file called 'message_shifted'.
 
-No documentation or testing is required.
+Prove this function works.
+
+Remember to add any additional test files to git and push them to the cloud so I can see your work.
 
 This will be graded out of 6.
 """
